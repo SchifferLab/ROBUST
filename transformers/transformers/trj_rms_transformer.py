7c1f4b296a740bf7b7d76c4f0f3b24a06501b6f6
FlorianLeidner <florian.leidner@umassmed.edu>
2020-04-29 10:36:17 -0400

    Fixed duplicated logger statement

--- transformers/transformers/trj_rms_transformer.py
+++ transformers/transformers/trj_rms_transformer.py
@@ -392,22 +392,6 @@ def main():
     else:
         cmsfile, trjtar, ligand_mae = args.infiles
 
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    # create file handler which logs even debug messages
-    fh = logging.FileHandler(os.path.join(os.getcwd(), ''.join(__file__.split('.')[:-1]) + '.log'))
-    fh.setLevel(logging.INFO)
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.ERROR)
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    # add the handlers to the logger
-    logger.addHandler(fh)
-    logger.addHandler(ch)
-
     structure_dict_list = [{'structure': {'code': prefix,
                                           'structure_id': 0},
                             'files': {'desmond_cms': cmsfile,
